d0576ebee631034e7180a1136213c4247266e8a8
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-03-04 15:09:08 +0800

    抽取方法 应对 过长的函数

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -3,8 +3,9 @@ package cn.xpbootcamp.tennis.game2;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame2 implements TennisGame {
-    public int P1point = 0;
-    public int P2point = 0;
+
+    private int playOnePoint = 0;
+    private int playTwoPoint = 0;
 
     public String P1res = "";
     public String P2res = "";
@@ -14,86 +15,120 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
-            score += "-All";
+        if (isPointSame()) {
+            score = getScoreWhenPointSame(score);
         }
-        if (P1point == P2point && P1point >= 3)
-            score = "Deuce";
+        if (isPlayOneGreateThanTwo()) {
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
+            if (playTwoPoint == 0) {
+                if (playOnePoint == 1) {
+                    P1res = "Fifteen";
+                }
+                if (playOnePoint == 2) {
+                    P1res = "Thirty";
+                }
+                if (playOnePoint == 3) {
+                    P1res = "Forty";
+                }
 
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+                P2res = "Love";
+                score = P1res + "-" + P2res;
+            }
+            if (playOnePoint < 4) {
+                if (playOnePoint == 2) {
+                    P1res = "Thirty";
+                }
+                if (playOnePoint == 3) {
+                    P1res = "Forty";
+                }
+                if (playTwoPoint == 1) {
+                    P2res = "Fifteen";
+                }
+                if (playTwoPoint == 2) {
+                    P2res = "Thirty";
+                }
+                score = P1res + "-" + P2res;
+            }
+            if (playTwoPoint >= 3) {
+                score = "Advantage player1";
+            }
+            if (playOnePoint >= 4 && playTwoPoint >= 0 && (playOnePoint - playTwoPoint) >= 2) {
+                score = "Win for player1";
+            }
         }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
+        if (playTwoPoint > 0 && playOnePoint == 0) {
+            if (playTwoPoint == 1) {
                 P2res = "Fifteen";
-            if (P2point == 2)
+            }
+            if (playTwoPoint == 2) {
                 P2res = "Thirty";
-            if (P2point == 3)
+            }
+            if (playTwoPoint == 3) {
                 P2res = "Forty";
+            }
 
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
 
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
+        if (playTwoPoint > playOnePoint && playTwoPoint < 4) {
+            if (playTwoPoint == 2) {
                 P2res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
+            }
+            if (playTwoPoint == 3) {
                 P2res = "Forty";
-            if (P1point == 1)
+            }
+            if (playOnePoint == 1) {
                 P1res = "Fifteen";
-            if (P1point == 2)
+            }
+            if (playOnePoint == 2) {
                 P1res = "Thirty";
+            }
             score = P1res + "-" + P2res;
         }
 
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (P2point > P1point && P1point >= 3) {
+        if (playTwoPoint > playOnePoint && playOnePoint >= 3) {
             score = "Advantage player2";
         }
 
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+        if (playTwoPoint >= 4 && playOnePoint >= 0 && (playTwoPoint - playOnePoint) >= 2) {
             score = "Win for player2";
         }
         return score;
     }
 
+    private boolean isPlayOneGreateThanTwo() {
+        return playOnePoint > playTwoPoint;
+    }
+
+    private String getScoreWhenPointSame(String score) {
+        if (playOnePoint < 4) {
+            if (playOnePoint == 0) {
+                score = "Love";
+            }
+            if (playOnePoint == 1) {
+                score = "Fifteen";
+            }
+            if (playOnePoint == 2) {
+                score = "Thirty";
+            }
+            score += "-All";
+        }
+        if (playOnePoint >= 3) {
+            score = "Deuce";
+        }
+        return score;
+    }
+
+    private boolean isPointSame() {
+        return playOnePoint == playTwoPoint;
+    }
+
     public void wonPoint(String player) {
         if (player.equals("player1")) {
-            P1point++;
+            playOnePoint++;
         } else {
-            P2point++;
+            playTwoPoint++;
         }
     }
 }
