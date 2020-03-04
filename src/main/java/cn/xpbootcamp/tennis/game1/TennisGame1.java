3320536b79f178f547ba4b20ad9a508ce4e38387
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-03-04 09:21:15 +0800

    抽取方法  减少过长的方法

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -18,54 +18,68 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        StringBuilder score = new StringBuilder();
-        int tempScore;
         if (M_SCORE1 == M_SCORE2) {
-            switch (M_SCORE1) {
+            return getScoreWhenTwoPlayerEqual();
+        } else if (M_SCORE1 >= 4 || M_SCORE2 >= 4) {
+            return getScoreWhenWinFourFrist();
+        } else {
+            return getScoreDefaultRule();
+        }
+    }
+
+    private String getScoreDefaultRule() {
+        int tempScore;
+        StringBuilder score = new StringBuilder();
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = M_SCORE1;
+            else {
+                score.append("-");
+                tempScore = M_SCORE2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = new StringBuilder("Love-All");
+                    score.append("Love");
                     break;
                 case 1:
-                    score = new StringBuilder("Fifteen-All");
+                    score.append("Fifteen");
                     break;
                 case 2:
-                    score = new StringBuilder("Thirty-All");
+                    score.append("Thirty");
                     break;
-                default:
-                    score = new StringBuilder("Deuce");
+                case 3:
+                    score.append("Forty");
                     break;
             }
-            return score.toString();
-        } else if (M_SCORE1 >= 4 || M_SCORE2 >= 4) {
-            int minusResult = M_SCORE1 - M_SCORE2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
-            return score.toString();
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = M_SCORE1;
-                else {
-                    score.append("-");
-                    tempScore = M_SCORE2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
-            return score.toString();
         }
+        return score.toString();
+    }
+
+    private String getScoreWhenWinFourFrist() {
+        StringBuilder score = new StringBuilder();
+        int minusResult = M_SCORE1 - M_SCORE2;
+        if (minusResult == 1) score = new StringBuilder("Advantage player1");
+        else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+        return score.toString();
+    }
+
+    private String getScoreWhenTwoPlayerEqual() {
+        StringBuilder score = new StringBuilder();
+        switch (M_SCORE1) {
+            case 0:
+                score = new StringBuilder("Love-All");
+                break;
+            case 1:
+                score = new StringBuilder("Fifteen-All");
+                break;
+            case 2:
+                score = new StringBuilder("Thirty-All");
+                break;
+            default:
+                score = new StringBuilder("Deuce");
+                break;
+        }
+        return score.toString();
     }
 }
